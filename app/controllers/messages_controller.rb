50876bdb0e118aaed1756983c613ebc9e1d0dc59
Julien Lemaire <julien_lemaire_62@yahoo.fr>
2025-11-25 12:49:12 +0100

    controller messages et show pokemons

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,12 @@
 class MessagesController < ApplicationController
   def create
+    @message = Message.new(message_params)
+    response = RubyLLM.chat.ask("Génère la réponse via ce texte: #{@message.text}").content
+    @message.save
 
+  private
+
+  def message_params
+    params.require(:message).permit(:text)
   end
 end
