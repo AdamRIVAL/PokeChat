fe7ec3ba20fe3af2975bb3ea4859fe775b940a65
camille hemet <camille.hemet@hotmail.fr>
2025-11-25 16:36:10 +0100

    with search bar

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,12 +2,13 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all.order(:number)
     @chats = Chat.all
+    if params[:query].present?
+    @pokemons = @pokemons.where("name ILIKE ?", "%#{params[:query]}%")
+    end
   end
 
   def show
     @pokemon = Pokemon.find(params[:id])
     # Add the amazing Julien's code for create a new message
-
   end
-
-
+end
