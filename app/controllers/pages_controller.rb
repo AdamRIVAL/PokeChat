5f0bd3f7c14d6a4e978bd1f07aef97bb35a34f0d
Quentin Luylier <qluylier@outlook.fr>
2025-11-25 13:13:45 +0100

    pokecard done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-
     # pour voir un pokémon test :
-    @pokemon = Pokemon.find_by(number: 14)
+    # @pokemon = Pokemon.find_by(number: 25)
     # FIN TEST
-
   end
 end
