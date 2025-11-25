27324e78288a2285756d3a8cd6776cd5750a61bf
Julien Lemaire <julien_lemaire_62@yahoo.fr>
2025-11-25 15:58:57 +0100

    routes, def create controller messages, clé api github, formulaire dans show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,6 +4,11 @@ class PokemonsController < ApplicationController
   end
 
   def show
-
+    @pokemon = Pokemon.find(params[:id])
+    @message = Message.new
+    @chat = Chat.find_by(user: current_user, pokemon: @pokemon)
+    if @chat.nil?
+      @chat = Chat.create(user: current_user, pokemon: @pokemon)
+    end
   end
 end
